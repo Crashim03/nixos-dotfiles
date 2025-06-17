150bb9afc932c13fa582577aac60993475030f8d
Alexandre Duarte <93451864+Crashim03@users.noreply.github.com>
2025-06-17 13:30:30 +0000

    Update home

--- home.nix
+++ home.nix
@@ -15,13 +15,84 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    obs-studio
     vscode
     discord
     ansible
     nextcloud-client
     spotify
     steam
+    zsh
+    dconf-editor
+    kitty
   ];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    autosuggestion.enable = true;
+
+    shellAliases = {
+      ll = "ls -la";
+      gs = "git status";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Crashim03";
+    userEmail = "alexteloduarte@gmail.com";
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/wm/keybindings" = {
+      close = ["<Super>Q"];
+      maximize = ["<Super>M"];
+      unmaximize = ["<Super>N"];
+
+      switch-to-workspace-left = ["<Super><Ctrl>Left" "<Super><Ctrl>KP_Left"];
+      switch-to-workspace-right = ["<Super><Ctrl>Right" "<Super><Ctrl>KP_Right"];
+      switch-to-workspace-down = ["<Super><Ctrl>Down" "<Super><Ctrl>KP_Down"];
+      switch-to-workspace-up = ["<Super><Ctrl>Up" "<Super><Ctrl>KP_Up"];
+
+      move-to-workspace-down = ["<Super><Shift>Down" "<Super><Shift>KP_Down"];
+      move-to-workspace-up = ["<Super><Shift>Up" "<Super><Shift>KP_Up"];
+      move-to-workspace-left = ["<Super><Shift>Left" "<Super><Shift>KP_Left"];
+      move-to-workspace-right = ["<Super><Shift>Right" "<Super><Shift>KP_Right"];
+
+      move-to-monitor-right = [];
+      move-to-monitor-left = [];
+      move-to-monitor-up = [];
+      move-to-monitor-down = [];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/browser/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/files/"
+      ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal" = {
+      name = "Launch Terminal";
+      command = "kitty zsh";
+      binding = "<Super>t";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/browser" = {
+      name = "Launch Browser";
+      command = "firefox";
+      binding = "<Super>b";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/files" = {
+      name = "Launch Files";
+      command = "nautilus";
+      binding = "<Super>f";
+    };
+  };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
